de5e92e55ef3f611a68c9bf10bd7b242ceb4ed48
Chiranjeevi <chiranjeeviaratakatla@gmail.com>
2019-12-27 20:55:24 +0530

    Basic Travel things code

---
+++ TravelThings/DAL/Interfaces/Iregistration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelThings.DAL.Interfaces
+{
+    public class Iregistration
+    {
+        internal interface 
+    }
+}
---
+++ TravelThings/Web_Forms/HomePage.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TravelThings.Web_Forms
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTraveler_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtName.Text.Trim()))
+            {
+                if(!string.IsNullOrEmpty(txtPhoneNo.Text.Trim()))
+                {
+
+                }
+            }
+        }
+    }
+}
